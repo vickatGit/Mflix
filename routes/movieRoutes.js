9e788e5b89a84732b348baa848fe98b5471cb5ab
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-09-25 21:44:54 +0530

    routes defined

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,4 +1,14 @@
 const router = require('express').Router()
 
-router.route("/")
+router.route("/banners")
+router.route("top_rated_movies/:page_no/:limit")
+router.route("new_releases/:page_no/:limit")
+router.route("genres")
+router.route("languages")
+router.route("by_genre/:genre/:page_no/:limit")
+router.route("by_language/:language/:page_no/:limit")
+router.route("search/:query/:page_no/:limit")
+router.route("get_movie/:movieId")
+router.route("get_comments/:movieId/:page_no/:limit")
+router.route("by_languages_genre/:language/:genre/:page_no/:limit")
 module.exports = router
