42716ed4a9419ce39a3a7875c4dac022a1b2871a
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-09-27 15:19:50 +0530

    top rated movies api

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const {
-    GetMoviesController
+    GetTopRatedMoviesController
 } = require('../controller/MovieController')
 
 router.route("/banners")
 router.route("/top_rated_movies/:page_no/:limit")
-router.route("/new_releases/:page_no/:limit").get(GetMoviesController)
+router.route("/new_releases/:page_no/:limit").get(GetTopRatedMoviesController)
 router.route("/genres")
 router.route("/languages")
 router.route("/by_genre/:genre/:page_no/:limit")
