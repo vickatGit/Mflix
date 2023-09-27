1167e27cdee5da3e238ba78eaa8f2102933509b3
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-09-27 15:51:16 +0530

    language filter is added to top rated movies api

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -4,8 +4,8 @@ const {
 } = require('../controller/MovieController')
 
 router.route("/banners")
-router.route("/top_rated_movies/:page_no/:limit")
-router.route("/new_releases/:page_no/:limit").get(GetTopRatedMoviesController)
+router.route("/top_rated_movies/:page_no/:limit/:lang").get(GetTopRatedMoviesController)
+router.route("/new_releases/:page_no/:limit")
 router.route("/genres")
 router.route("/languages")
 router.route("/by_genre/:genre/:page_no/:limit")
