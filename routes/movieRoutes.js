7c480fde366df9bf47d9cd3564bc0b4f5a3111b1
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-10-02 00:02:43 +0530

    documentaion done for get_movie api

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,3 +1,167 @@
+
+/**
+ * @swagger
+ * components:
+ *  schemas :
+ *    Movie:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        plot:
+ *          type: string
+ *        genres:
+ *          type: array
+ *          items:
+ *            type: string
+ *        runtime:
+ *          type: integer
+ *        rated:
+ *           type: string
+ *        cast:
+ *           type: array
+ *           items:
+ *             type: string
+ *        num_mflix_comments:
+ *           type: integer
+ *        poster:
+ *          type: string
+ *        title:
+ *          type: string
+ *        fullplot:
+ *          type: string
+ *        languages:
+ *           type: array
+ *           items:
+ *             type: string
+ *        released:
+ *          type: string
+ *        directors:
+ *          type: array
+ *          items:
+ *            type: string
+ *        writers:
+ *          type: array
+ *          items:
+ *            type: string
+ *        awards:
+ *          $ref : '#/components/schemas/Award'
+ *          
+ *        lastupdated:
+ *           type: string
+ *        year:
+ *          type: integer
+ *        imdb:
+ *          $ref : '#/components/schemas/Imdb'
+ *        countries:
+ *          type: array
+ *          items:
+ *            type: string
+ *        type:
+ *            $ref : '#/components/schemas/Tomatoes'
+ *        comments : 
+ *          type : object
+ *          items : 
+ *            $ref : '#/components/schemas/Comment'
+ *    Comment:
+ *      type: object
+ *      properties : 
+ *        rating:
+ *          type: number
+ *        votes:
+ *          type: integer
+ *        id:
+ *          type: integer
+ *    Imdb : 
+ *      type: object
+ *      properties:
+ *        rating:
+ *          type: number
+ *        votes:
+ *          type: integer
+ *        id:
+ *          type: integer
+ *    Award:
+ *      type: object
+ *      properties:
+ *        wins:
+ *          type: integer
+ *        nominations:
+ *          type: integer
+ *        text:
+ *          type: string
+ *        _id:
+ *          type: string
+ *    TomatoesViewer:
+ *      type: object
+ *      properties:
+ *        rating:
+ *          type: number
+ *        numReviews:
+ *          type: integer
+ *        meter:
+ *          type: integer
+ *    TomatoesCritic:
+ *      type: object
+ *      properties:
+ *        rating:
+ *          type: number
+ *        numReviews:
+ *          type: integer
+ *        meter:
+ *          type: integer
+ *    Tomatoes:
+ *      type: object
+ *      properties:
+ *        viewer:
+ *          $ref: '#/components/schemas/TomatoesViewer'
+ *        dvd:
+ *          type: string
+ *        critic:
+ *          $ref: '#/components/schemas/TomatoesCritic'
+ *        lastUpdated:
+ *          type: string
+ *        consensus:
+ *          type: string
+ *        rotten:
+ *          type: integer
+ *        production:
+ *          type: string
+ *        fresh:
+ *          type: integer
+ */
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Movies
+ */
+
+/**
+ * @swagger
+ * /mflix/get_movie/{movieId}:
+ *   get:
+ *     summary: Returns the Movie
+ *     tags: [Movies]
+ *     parameters: 
+ *       - in: path
+ *         name: movieId
+ *         schema: 
+ *           type: string
+ *         required: true
+ *         description: The Movie Id
+ *     responses:
+ *       200:
+ *         description: Movie with specified id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
+ *       404:
+ *         description: Movie Not Found
+ */
+
 const router = require('express').Router()
 const {
     GetTopRatedMoviesController,
@@ -29,3 +193,6 @@ router.route("/comments/:page_no/:limit/:movieId").get(GetMovieCommentsControlle
 
 router.route("/post_comment/:movieId").post(PostCommentController)
 module.exports = router
+
+
+
